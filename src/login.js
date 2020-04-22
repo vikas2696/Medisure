aaf09094b8e7a997361edcd52b84a4541cdc7697
Vikas Mehra <vickeyvikasmehra@gmail.com>
2020-04-22 21:35:47 +0530

    with login changed

--- src/login.js
+++ src/login.js
@@ -44,7 +44,7 @@ app.post('/auth', function(request, response) {
 app.get('/home', function(request, response) {
 	if (request.session.loggedin) {
 		//response.send('Welcome back, ' + request.session.username + '!');
-		response.sendFile(path.join(__dirname + './view_details.html'));
+		response.sendFile(path.join(__dirname + '/view_details.html'));
 	} else {
 		response.send('Please login to view this page!');
 	}
